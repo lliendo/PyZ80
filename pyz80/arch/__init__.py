f126c7e9e21b069a7e2d78f62688ffe392ca78bc
Lucas Liendo <mindmaster@gmail.com>
2015-02-15 21:40:24 -0300

    - Added license.

--- pyz80/arch/__init__.py
+++ pyz80/arch/__init__.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of PyZ80.
+
+PyZ80 is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PyZ80 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+Lesser GNU General Public License for more details.
+
+You should have received a copy of the Lesser GNU General Public License
+along with PyZ80. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2014 Lucas Liendo.
+"""
+
 NIBBLE_SIZE = 4
 BYTE_SIZE = 8
 WORD_SIZE = 16
